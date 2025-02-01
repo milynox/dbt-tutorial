cfbee7bfedca22592a8667c590ebb2032a53aadb
Nhan Nguyen <thanhnhan16121789@gmail.com>
2025-02-01 07:02:31 +0000

    update

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,4 +1,7 @@
-WITH paid_orders as (select Orders.ID as order_id,
+WITH paid_orders as (
+    select
+
+        Orders.ID as order_id,
         Orders.USER_ID    as customer_id,
         Orders.ORDER_DATE AS order_placed_at,
             Orders.STATUS AS order_status,
@@ -6,22 +9,32 @@ WITH paid_orders as (select Orders.ID as order_id,
         p.payment_finalized_date,
         C.FIRST_NAME    as customer_first_name,
             C.LAST_NAME as customer_last_name
+
     FROM {{ source('jaffle_shop', 'orders') }} as Orders
-    left join (select ORDERID as order_id, max(CREATED) as payment_finalized_date, sum(AMOUNT) / 100.0 as total_amount_paid
-from {{ source('stripe', 'payments') }}
-where STATUS <> 'fail'
-group by 1) p ON orders.ID = p.order_id
+    left join (
+        select 
+        
+        ORDERID as order_id, max(CREATED) as payment_finalized_date, sum(AMOUNT) / 100.0 as total_amount_paid
+
+        from {{ source('stripe', 'payments') }}
+        where STATUS <> 'fail'
+        group by 1
+    ) p ON orders.ID = p.order_id
 left join {{ source('jaffle_shop', 'customers') }} C on orders.USER_ID = C.ID ),
 
-customer_orders 
-    as (select C.ID as customer_id
+customer_orders as (
+    select 
+    
+    C.ID as customer_id
         , min(ORDER_DATE) as first_order_date
         , max(ORDER_DATE) as most_recent_order_date
         , count(ORDERS.ID) AS number_of_orders
+
     from {{ source('jaffle_shop', 'customers') }} C 
     left join {{ source('jaffle_shop', 'orders') }} as Orders
     on orders.USER_ID = C.ID 
-    group by 1)
+    group by 1
+    )
 
 select
     p.*,
