7cc183a8a15e6987b9a2fcadd33740651d4b407b
Nhan Nguyen <thanhnhan16121789@gmail.com>
2025-02-02 04:23:40 +0000

    Create staging model and update references to point to staging tables

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,36 +1,36 @@
 with
     -- Import CTEs
     -- See all the tables used in this file at the top
-    customers as (select * from {{ source("jaffle_shop", "customers") }}),
+    customers as (select * from {{ ref('stg_jaffle_shop__customers') }}),
 
-    orders as (select * from {{ source("jaffle_shop", "orders") }}),
+    orders as (select * from {{ ref('stg_jaffle_shop__orders') }}),
 
-    payments as (select * from {{ source("stripe", "payments") }}),
+    payments as (select * from {{ ref('stg_stripe__payments') }}),
 
     -- Logical CTEs
     completed_payments as (  -- total amount has been paid for an order
         select
-            orderid as order_id,
-            max(created) as payment_finalized_date,
-            sum(amount) / 100.0 as total_amount_paid
+            order_id,
+            max(payment_created_at) as payment_finalized_date,
+            sum(payment_amount) as total_amount_paid
         from payments
-        where status <> 'fail'
+        where payment_status <> 'fail'
         group by 1
     ),
 
     paid_orders as (  -- detail info of these orders 
         select
-            orders.id as order_id,
-            orders.user_id as customer_id,
-            orders.order_date as order_placed_at,
-            orders.status as order_status,
+            orders.order_id,
+            orders.customer_id,
+            orders.order_placed_at,
+            orders.order_status,
             completed_payments.total_amount_paid,
             completed_payments.payment_finalized_date,
-            customers.first_name as customer_first_name,
-            customers.last_name as customer_last_name
+            customers.customer_first_name,
+            customers.customer_last_name
         from orders
-        left join completed_payments on orders.id = completed_payments.order_id
-        left join customers on orders.user_id = customers.id
+        left join completed_payments on orders.order_id = completed_payments.order_id
+        left join customers on orders.customer_id = customers.customer_id
     ),
 
     -- Final CTEs
@@ -71,8 +71,6 @@ with
             ) as fdos
 
         from paid_orders
-        -- left join customer_orders as c using (customer_id)
-        -- left outer join x on x.order_id = p.order_id
         order by paid_orders.order_id
     )
 
