391fe98ad67b4b907339b33efc9d7c8d2ca99287
Nhan Nguyen <thanhnhan16121789@gmail.com>
2025-02-02 02:13:05 +0000

    Optimize queries using window func and remove unnecessary joins

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -8,7 +8,7 @@ with
     payments as (select * from {{ source("stripe", "payments") }}),
 
     -- Logical CTEs
-    completed_payments as (
+    completed_payments as (  -- total amount has been paid for an order
         select
             orderid as order_id,
             max(created) as payment_finalized_date,
@@ -18,7 +18,7 @@ with
         group by 1
     ),
 
-    paid_orders as (
+    paid_orders as (  -- detail info of these orders 
         select
             orders.id as order_id,
             orders.user_id as customer_id,
@@ -33,45 +33,47 @@ with
         left join customers on orders.user_id = customers.id
     ),
 
-    customer_orders as (
-        select
-            customers.id as customer_id,
-            min(orders.order_date) as first_order_date,
-            max(orders.order_date) as most_recent_order_date,
-            count(orders.id) as number_of_orders
-        from customers
-        left join orders on orders.user_id = customers.id
-        group by 1
-    )
-
     -- Final CTEs
     final as (
         select
-            p.*,
-            row_number() over (order by p.order_id) as transaction_seq,
+            paid_orders.*,
+            row_number() over (order by paid_orders.order_id) as transaction_seq,
             row_number() over (
-                partition by customer_id order by p.order_id
+                partition by customer_id order by paid_orders.order_id
             ) as customer_sales_seq,
+
+            -- new vs returning customer for each customer
             case
-                when c.first_order_date = p.order_placed_at then 'new' else 'return'
+                when
+                    (
+                        row_number() over (
+                            partition by paid_orders.customer_id
+                            order by paid_orders.order_placed_at, paid_orders.order_id
+                        )
+                        = 1
+                    )
+                then 'new'
+                else 'return'
             end as nvsr,
-            x.clv_bad as customer_lifetime_value,
-            c.first_order_date as fdos
-        from paid_orders p
-        left join customer_orders as c using (customer_id)
-        left outer join
-            (
-                select p.order_id, sum(t2.total_amount_paid) as clv_bad
-                from paid_orders p
-                left join
-                    paid_orders t2
-                    on p.customer_id = t2.customer_id
-                    and p.order_id >= t2.order_id
-                group by 1
-                order by p.order_id
-            ) x
-            on x.order_id = p.order_id
-        order by order_id
+
+            -- Customer lifetime value for each order
+            -- Với từng đơn hàng của từng khách, tính tổng doanh thu của các đơn trước đó.
+            sum(paid_orders.total_amount_paid) over (
+                partition by paid_orders.customer_id
+                order by paid_orders.order_id asc
+                rows between unbounded preceding and current row
+            ) as customer_lifetime_value,
+
+            -- first day of sale
+            first_value(paid_orders.order_placed_at) over (
+                partition by paid_orders.customer_id
+                order by paid_orders.order_placed_at
+            ) as fdos
+
+        from paid_orders
+        -- left join customer_orders as c using (customer_id)
+        -- left outer join x on x.order_id = p.order_id
+        order by paid_orders.order_id
     )
 
 -- Simple Select Statement
